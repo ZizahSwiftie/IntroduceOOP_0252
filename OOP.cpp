bf2fc546e382c04d3a18cd61ba31e35d6172bde1
ZizahSwiftie <azizahulnha@gmail.com>
2025-04-28 19:11:56 +0700

    luas persegipanjang

--- OOP.cpp
+++ OOP.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 using namespace std;
 
-class planet{
+class planet
+{
     private:
-           float gravitasi;
+        float gravitasi;
+
     public:
         string nama;
         float diameter;
         
         void displaydata(){
             cout << "halo aku planet "<< nama << ", ukuranku "<< diameter <<", gravitasiku "<< gravitasi<< endl;
-        }  
+        } 
+    void inputdata(){
+        cout << "input nama planet: ";
+        cin >> nama;
+        cout << "ukuranku: ";
+        cin >> diameter;
+        cout << "gravitasiku: ";
+        cin >> gravitasi;
+    }
 
 }; 
 int main()
 {
+    planet venus;
+    venus.inputdata();
+    cout << "namanya adlah: " << venus.nama << endl;
+    cout << "diameter adalah: " << venus.diameter << endl;
+
+    venus.displaydata();
 
 }
